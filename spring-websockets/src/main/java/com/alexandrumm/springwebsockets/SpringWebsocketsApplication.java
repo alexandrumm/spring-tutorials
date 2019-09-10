d788d013bfd9c91488d7a37da5228e9f41f8c82f
Alexandru Muntean <alexandru.muntean@accesa.eu>
2019-09-10 21:48:14 +0300

    Init project

---
+++ spring-websockets/src/main/java/com/alexandrumm/springwebsockets/SpringWebsocketsApplication.java
@@ -0,0 +1,13 @@
+package com.alexandrumm.springwebsockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsocketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebsocketsApplication.class, args);
+	}
+
+}
